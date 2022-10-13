27cc88233b221e7f7a2ed71ed4a914a55201aa0f
Dharmendra <dharmendrasingh75039@gmail.com>
2022-10-13 13:29:14 +0530

    Installed express-ejs-layouts &setup layouts and partial

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 //use express router
 app.use('/', require('./routes'));
 
 //set up the view engine
+app.set('view engine', 'ejs');
 app.set("views", './views');
 
 
