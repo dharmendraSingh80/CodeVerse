ac12984fd4837432746f763330f024da5e8cabc3
Dharmendra <dharmendrasingh75039@gmail.com>
2022-10-20 13:02:32 +0530

    Authentication using Passport js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,6 +7,11 @@ module.exports.profile = function(req, res){
 
 //render the sign up page
 module.exports.signUp = function(req, res){
+
+     if(req.isAuthenticated()){
+         return res.redirect('/users/profile');
+     }
+
      return res.render('user_sign_up', {
           title:'Codeial | Sign Up'
      })
@@ -15,6 +20,11 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+
+     if(req.isAuthenticated()){
+         return res.redirect('/users/profile');
+     }
+
      return res.render('user_sign_In', {
           title:'Codeial | Sign In'
      })
@@ -48,5 +58,12 @@ module.exports.create = function(req, res){
 
 //sign in create the session for the user
 module.exports.createSession = function(req, res){
-     //TODO later
+     return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res){
+     req.logout(function(err) {
+          if (err) { return next(err); }
+     return res.redirect('/');
+     });
 }
